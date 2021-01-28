b5070c8ae2e18f9c13ba54ee106f0f59f3343509
vladmorari <morariv67@gmail.com>
2021-01-28 23:48:05 +0200

    errors

--- meteo-app/src/components/Meteo.js
+++ meteo-app/src/components/Meteo.js
@@ -1,26 +1,60 @@
-import React from "react";
+import React, { useReducer } from "react";
 
 function Meteo() {
-  const [meteoData, setMeteoData] = React.useState({});
-  const [query, setQuery] = React.useState("chisinau");
-  const [inputData, setInputData] = React.useState("");
+  // const [meteoData, setMeteoData] = React.useState({});
+  // const [query, setQuery] = React.useState("chisinau");
+  // const [inputData, setInputData] = React.useState("");
+
+  const dataReducer = (state, action) => {
+    switch (action.type) {
+      case "CHANGE_INPUT":
+        return {
+          ...state,
+          input: action.payload.text,
+        };
+      case "FETCH_DATA":
+        return {
+          ...state,
+          meteo: action.payload.text,
+        };
+      case "METEO_DATA":
+        return {
+          ...state,
+          city: action.payload.text,
+        };
+      default:
+        return state;
+    }
+  };
+  const initialState = {
+    meteo: {},
+    city:"chisinau",
+    input:""
+
+  };
+  const [state, dispatch] = useReducer(dataReducer, initialState);
+
+  function cityName(e) {
+    dispatch({
+      type: "CHANGE_INPUT",
+      payload: { input: e.target.value },
+    });
+  }
 
   React.useEffect(() => {
     const fetchMeteo = async () => {
       const requestData = await fetch(
-        `http://api.openweathermap.org/data/2.5/weather?q=${query}&appid=60863f88c34e274c330b04f9cdd90e7d&units=metric`
+        `http://api.openweathermap.org/data/2.5/weather?q=${state.city}&appid=60863f88c34e274c330b04f9cdd90e7d&units=metric`
       );
       const responseData = await requestData.json();
-      console.log("->", responseData);
-      setMeteoData(responseData);
+      dispatch({
+        type: "FETCH_DATA",
+        payload: { meteo: responseData },
+      });
+      // setMeteoData(responseData);
     };
     fetchMeteo();
-  }, [query]);
-
-  function cityName(e) {
-    console.log(e.target.value);
-    setInputData(e.target.value);
-  }
+  }, [state.city]);
 
   return (
     <div>
@@ -29,23 +63,27 @@ function Meteo() {
       <div>
         <button
           onClick={(e) => {
-            setQuery(inputData);
+            dispatch({
+              type: "METEO_DATA",
+              payload: { city: "chisinau" },
+            });
+            // setQuery(inputData);
           }}
         >
           Show
         </button>
       </div>
-      {Object.keys(meteoData).length === 0 ? (
+      {Object.keys(state.meteo).length === 0 ? (
         <h1>Loading...</h1>
-      ) : meteoData.cod === "404" || meteoData.cod === "400" ? (
-        <h1>{meteoData.message}...</h1>
+      ) : state.meteo.cod === "404" || state.meteo.cod === "400" ? (
+        <h1>{state.meteo.message}...</h1>
       ) : (
         <div>
           <div>
-            Country: {JSON.stringify(meteoData.sys.country)} ,
-            {JSON.stringify(meteoData.name)}
+            Country: {JSON.stringify(state.meteo.sys.country)} ,
+            {JSON.stringify(state.meteo.name)}
           </div>
-          <div> Temperature: {JSON.stringify(meteoData.main.temp)} ℃</div>
+          <div> Temperature: {JSON.stringify(state.meteo.main.temp)} ℃</div>
         </div>
       )}
     </div>
